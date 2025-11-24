c210b5a5201684ebd6095e2c979326bb4b2053f6
gankerV <134499303+gankerV@users.noreply.github.com>
2025-11-24 07:55:28 +0700

    3

--- src/main/java/com/example/demo/repo/UserRepo.java
+++ src/main/java/com/example/demo/repo/UserRepo.java
@@ -4,6 +4,10 @@ import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepo extends JpaRepository<User,Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    List<User> findByNameContainingIgnoreCase(String name);
+
+    List<User> findByPhone(String phone);
+
 }
